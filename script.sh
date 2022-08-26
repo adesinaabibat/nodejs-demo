b57acea0dd0a1cce577c47877fb7fb902b6498ff
ab4ever <63836894+ab4ever@users.noreply.github.com>
2022-08-25 17:20:46 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-ROLE_ARN=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.executionRoleArn`
+ROLE_ARN="aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.executionRoleArn"
 echo "ROLE_ARN= " $ROLE_ARN
 
 FAMILY=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.family`
